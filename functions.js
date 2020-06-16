458d25b528d5701c38510d79e72e843867f9a4bf
kalcalen <kalcalen@gmail.com>
2020-06-15 22:02:27 -0700

    created filter function

--- functions.js
+++ functions.js
@@ -1,1 +1,21 @@
-// Your functions go here!
+// Impletment the filter method on a prototype
+function filterMethod(input) {
+   var randomArr = [35, 87, 22, 96, 22, 66, 8, input];
+
+   Array.prototype.userFilter = function (oddNumbers) {
+      var newArray = [];
+      // Empty array that numbers will be pushed into
+      for (let i = 0; i <= this.length; i++) {
+         if (oddNumbers(this[i])) {
+            newArray.push(this[i]);
+         }
+      }
+      return newArray;
+   };
+
+   // filteredArray takes array filters out all odd numbers
+   var filteredArray = randomArr.userFilter(function (item) {
+      return item % 2 === 1;
+   });
+   return filteredArray;
+}
